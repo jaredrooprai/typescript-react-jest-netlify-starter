8d2a284f88680246593976806e7f7ab801edb49d
Jared Rooprai <jared.rooprai@showpass.com>
2020-10-09 01:32:09 -0600

    contextx to jest config

--- jest.config.js
+++ jest.config.js
@@ -8,5 +8,6 @@ module.exports = {
     '^@pages(.*)$': '<rootDir>/src/pages$1',
     '^@components(.*)$': '<rootDir>/src/components$1',
     '^@utils(.*)$': '<rootDir>/src/utils$1',
+    '^@contexts(.*)$': '<rootDir>/src/contexts$1',
   },
 };
